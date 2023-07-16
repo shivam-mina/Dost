4ddf8284430b7b3620402efd0ca4274f642679d0
Shivam Mina <shivammeenashiva@gmail.com>
2023-07-16 20:01:04 +0530

    Added Cookie verification route

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,7 +1,9 @@
 const { SignUp, Login } = require('../controllers/AuthController')
+const { verifyUser } = require('../middlewares/AuthMiddleware')
 const router = require('express').Router()
 
 router.post('/signup', SignUp)
 router.post('/login', Login)
+router.post('/', verifyUser)
 
 module.exports = router
