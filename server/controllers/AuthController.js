e492a3f41ddb92879e07e82eed1f0086ad82ad1f
Shivam Mina <shivammeenashiva@gmail.com>
2023-07-16 18:07:47 +0530

    Added Login Route

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -17,7 +17,7 @@ module.exports.SignUp = async (req, res, next) => {
 
     const user = await User.create({ username, email, password, createdAt })
     const token = createSecretToken(user._id)
-
+    //????
     res.cookie('token', token, {
       withCredentials: true,
       httpOnly: false,
@@ -25,6 +25,37 @@ module.exports.SignUp = async (req, res, next) => {
 
     res.status(201).json({ message: 'User Signed up', success: true, user })
 
+    next()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.json({ message: 'No empty field allowed' })
+    }
+
+    const user = await User.findOne({ email })
+    if (!user) {
+      return res.json({ message: 'Incorrect email' })
+    }
+
+    /* checking password */
+    const auth = await bcrypt.compare(password, user.password)
+    if (!auth) {
+      return res.json({ message: 'Wrong password' })
+    }
+
+    const token = createSecretToken(user._id)
+    res.cookie('token', token, {
+      withCredentials: true,
+      httpOnly: false,
+    })
+
+    res.status(201).send({ message: 'Logged in Successfully', success: true })
     next()
   } catch (error) {
     console.log(error)
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,6 +1,7 @@
-const { SignUp } = require('../controllers/AuthController')
+const { SignUp, Login } = require('../controllers/AuthController')
 const router = require('express').Router()
 
 router.post('/signup', SignUp)
+router.post('/login', Login)
 
 module.exports = router
